cfb6424eacfbde7c8b9a9e18758870be518d3139
nachofarah <nachofarah07@gmail.com>
2026-07-27 14:53:17 +0000

    creamos un apartado categoria, y añadimos un apartado para subir fotos dentro de los productos

--- producto/admin.py
+++ producto/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Producto
+from producto.models import categoria
 
 admin.site.register(Producto)
+admin.site.register(categoria)
 
 # Register your models here.
--- producto/models.py
+++ producto/models.py
@@ -1,14 +1,36 @@
 from django.db import models
 
+class categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True) 
+    slug = models.SlugField (unique=True)
+
+    class Meta:
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categorias'
+        ordering = ['nombre']
+
+    def __str__(self):
+        return self.nombre
+
 class Producto(models.Model):
+    categoria = models.ForeignKey(
+        categoria,
+        on_delete=models.CASCADE,
+        related_name='productos',
+        null=True,
+        blank=True
+    )
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_actualizacion = models.DateTimeField(auto_now=True)
+    imagen = models.ImageField(upload_to='MEDIA/', null=True, blank=True)
+    activo = models.BooleanField(default=True)
 
-def __str__(self):
-    return self.nombre
+
+    def __str__(self):
+        return self.nombre
 
 # Create your models here.
